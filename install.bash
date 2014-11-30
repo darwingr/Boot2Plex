a03835ce1ed1fcd1734d387a172da5c018db0e00
Darwin Groskleg <darwingroskleg@gmail.com>
2014-11-30 08:56:17 -0500

    Changes to using full directory paths where possible. Removes the now redundant cd command. Running the script will no longer put us somewhere else.

--- install.bash
+++ install.bash
@@ -3,9 +3,8 @@
 launchctl unload /Library/LaunchDaemons/com.boot2plex.plexserverd.plist
 
 cp -f com.boot2plex.plexserverd.plist /Library/LaunchDaemons
-cd /Library/LaunchDaemons
-chown root:wheel com.boot2plex.plexserverd.plist
-chmod 644 com.boot2plex.plexserverd.plist
+chown root:wheel /Library/LaunchDaemons/com.boot2plex.plexserverd.plist
+chmod 644 /Library/LaunchDaemons/com.boot2plex.plexserverd.plist
 
-launchctl load -w com.boot2plex.plexserverd.plist
+launchctl load -w /Library/LaunchDaemons/com.boot2plex.plexserverd.plist
 launchctl start com.boot2plex.plexserverd
