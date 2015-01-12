a2703f008391ca59cc51b70f1cf8b5e64ab2d268
Darwin Groskleg <darwingroskleg@gmail.com>
2015-01-12 02:56:09 -0500

    runs the install script at the end of the new installer script

--- installer.bash
+++ installer.bash
@@ -4,4 +4,7 @@ curl -LOk https://github.com/darwingr/ServiceAccountMaker/archive/master.zip && 
 chmod +x ServiceAccountMaker-master/mk_service_user.sh
 ./ServiceAccountMaker-master/mk_service_user.sh plex
 
-curl -LOk https://downloads.plex.tv/plex-media-server/0.9.11.7.803-87d0708/PlexMediaServer-0.9.11.7.803-87d0708-OSX.zip 
+curl -LOk https://downloads.plex.tv/plex-media-server/0.9.11.7.803-87d0708/PlexMediaServer-0.9.11.7.803-87d0708-OSX.zip
+
+chmod +x install.bash
+./install.bash
