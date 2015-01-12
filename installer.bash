1d358ac56eb7b0c0a3b7fe3c1141243d4a9f671d
Darwin Groskleg <darwingroskleg@gmail.com>
2015-01-12 03:47:36 -0500

    removes ugly download of server app adds line to simply copy the server app from the default application location

--- installer.bash
+++ installer.bash
@@ -4,7 +4,7 @@ curl -LOk https://github.com/darwingr/ServiceAccountMaker/archive/master.zip && 
 chmod +x ServiceAccountMaker-master/mk_service_user.sh
 ./ServiceAccountMaker-master/mk_service_user.sh plex
 
-curl -LOk https://downloads.plex.tv/plex-media-server/0.9.11.7.803-87d0708/PlexMediaServer-0.9.11.7.803-87d0708-OSX.zip
+sudo ditto /Applications/Plex\ Media\ Server.app /usr/local/plex/Plex\ Media\ Server.app
 chgrp _plex /usr/local/plex/Plex\ Media\ Server.app
 chmod 775 /usr/local/plex/Plex\ Media\ Server.app
 
